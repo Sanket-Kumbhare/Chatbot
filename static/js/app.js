514632eca76e77579178616502f8ef516f232c2b
Sanket-Kumbhare <sanketkumbhare1511@gmail.com>
2021-04-27 05:25:54 +0530

    brain folder added for trash talk

--- static/js/app.js
+++ static/js/app.js
@@ -2,10 +2,10 @@ var $ = new BaseJS();
 
 $.ready(function() {
 	var result = $.select("#result");
+	var message = $.select("#message");
 		
-	$.select("#message").on("keyup").call(function(e) {
-		if (e.keyCode == 13 && this.value.trim() !== "") {
-			$.http("/" + encodeURI(this.value)).get().ready(function(res) {
+	function reply() {
+		$.http("/" + message[0].value).get().ready(function(res) {
 				if (res.readyState === 4 && res.status == 200) {
 					txt = res.responseText;
 					if (txt.trim() === "") {
@@ -16,29 +16,20 @@ $.ready(function() {
 				}
 			});
 
-			result.append("<div class='you'><span class=' bg-primary text-white'>" + this.value + "</span></div>");
-			this.value = "";
+			result.append("<div class='you'><span class=' bg-primary text-white'>" + message[0].value + "</span></div>");
+			message[0].value = "";
 			result[0].scrollTop = result[0].scrollHeight;
+	}
+
+	$.select("#message").on("keyup").call(function(e) {
+		if(e.keyCode == 13 && message[0].value.trim() !== "") {
+			reply();
 		}
 	});
 
-  	var message = document.getElementById('message')
-	var sendBtn = document.getElementById('send');
-	sendBtn.addEventListener('click', function() {
-		$.http("/" + message.value).get().ready(function(res) {
-				if (res.readyState === 4 && res.status == 200) {
-					txt = res.responseText;
-					if (txt.trim() === "") {
-						txt = "?";
-					}
-					result.append("<div class='bot'><img class='mr-2' src='/static/media/kgce1.png' style='width: 30px; height: 30px'><span>" + txt + "</span></div>");
-					result[0].scrollTop = result[0].scrollHeight;
-				}
-			});
-
-		result.append("<div class='you'><span class=' bg-primary text-white'>" + message.value + "</span></div>");
-		message.value = "";
-		result[0].scrollTop = result[0].scrollHeight;
+	$.select("#send").on("click").call(function() {
+		if(message[0].value.trim() !="") {
+			reply();
+		}
 	});
-
 });
